1ea63f1c39edb0d1e6a1f8d80d81b3eeed1c60b8
siryu <siryu2409@gmail.com>
2022-05-31 16:34:35 +0900

    temp
    temp

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script contains PlayerContext and Database, Network data
+public class Player : MonoBehaviour
+{
+    PlayerCharacter playerCharacter;
+    PlayerCharacterController PlayerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
