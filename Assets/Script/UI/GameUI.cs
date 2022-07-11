f09b3ed511ec0171a2de37e528632827da9f7aa1
coco1337 <scar3517@gmail.com>
2022-07-11 17:47:02 +0900

    Add In-game UI Interface functions

--- Assets/Script/UI/GameUI.cs
+++ Assets/Script/UI/GameUI.cs
@@ -2,8 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public sealed class GameUI : MonoBehaviour
 {
+  public static GameUI Instance;
+
   [SerializeField] private GameObject resultPanel;
+
+  [SerializeField] private TextMeshProUGUI timer;
+  [SerializeField] private TextMeshProUGUI weapon;
+  [SerializeField] private TextMeshProUGUI magazine;
+
+  private void Start()
+  {
+    Instance ??= this;
+  }
+
+  public void UpdateTimer()
+  {
+    timer.text = "";
+  }
+
+  public void UpdateWeapon(string weaponName)
+  {
+    weapon.text = weaponName;
+  }
+
+  public void UpdateMagazine(string remainBullet)
+  {
+    // TODO : magazine무기에따라 최대 양
+    magazine.text = remainBullet;
+  }
 }
